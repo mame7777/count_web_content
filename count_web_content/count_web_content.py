5b1833910bd802d87822a1c9a146bb2b31634dc5
mame7777 <86618354+mame7777@users.noreply.github.com>
2024-10-22 10:18:16 +0900

    [update] 除外コンテンツの設定を追加

--- count_web_content/count_web_content.py
+++ count_web_content/count_web_content.py
@@ -10,24 +10,28 @@ from bs4 import BeautifulSoup
 
 def count_pages(
     root_url: str,
-    additional_url: list[str] = [],
-    # exclude_url: list[str] = [],
+    additional_url: list[str] = None,
+    exclude_url: list[str] = None,
     sleep_sec: int = 0.1,
     is_print_working: bool = False,
     output_url_file_name: str = "",
-) -> Tuple[int, int, dict[str, int]]:
+) -> Tuple[int, int, dict[str, int] | None, dict[str, int] | None]:
     """ページのコンテンツをカウントする
 
     Args:
         root_url (str): 検索したいルートのURL
         additional_url (list[str], optional): 個別にコンテンツ数をカウントしたいURL(root_url配下). Defaults to [].
+        exclude_url (list[str], optional): カウント対象外のURL. Defaults to [].
         sleep_sec (int, optional): スクレイピングの時間間隔. Defaults to 0.1.
         is_print_working (bool, optional): 実行中の状態を出力するかどうか. Defaults to False.
         output_url_file_name (str, optional): 検索したルートURL配下のファイル一覧を出力するファイル先. Defaults to "".
 
     Returns:
-        Tuple[int, int, dict[str, int]]: [hrefのリンク先数, root_url配下のhrefのリンク先数, additional_urlで指定したパス配下のhrefのリンク先数, root_url配下のhrefのリスト]
-        リンク先の数は全て一意のURLでカウントしている
+        Tuple[int, int, dict[str, int], dict[str, int]]: 前から順に
+        hrefのリンク先数, root_url配下のhrefのリンク先数,
+        additional_urlで指定したパス配下のhrefのリンク先数, exclude_urlで指定したパス配下のhrefのリンク先数,　が戻る．
+        リンク先の数は全て一意のURLでカウントしている.
+        戻り値の後ろの2つの辞書は、指定がない場合はNoneが返る.
     """
     not_completed = list()
     not_completed.append(root_url)
@@ -37,18 +41,36 @@ def count_pages(
     in_root_page_count = 0
 
     additional_url_count_dict = dict()
-    for key in additional_url:
-        additional_url_count_dict[key] = 0
+    if additional_url != None:
+        for key in additional_url:
+            additional_url_count_dict[key] = 0
+
+    exclude_url_count_dict = dict()
+    if exclude_url != None:
+        for key in exclude_url:
+            exclude_url_count_dict[key] = 0
 
     while len(not_completed) > 0:
         sleep(sleep_sec)
         current_url = not_completed.pop()
+
+        is_continue = False
+        for key in exclude_url:
+            if current_url.startswith(key):
+                exclude_url_count_dict[key] += 1
+                is_continue = True
+                continue
+
+        if is_continue:
+            continue
+
         try:
             response = requests.get(current_url, timeout=6.0)
         except requests.exceptions.RequestException as e:
             print(e)
             print("Error: " + current_url)
             continue
+
         if response.status_code != 200:
             continue
 
@@ -101,4 +123,15 @@ def count_pages(
         with open(output_url_file_name, "w", encoding="utf-8") as f:
             for url in completed_list:
                 print(url, file=f)
-    return page_count, in_root_page_count, additional_url_count_dict
+
+    if len(additional_url_count_dict) == 0:
+        additional_url_count_dict = None
+    if len(exclude_url_count_dict) == 0:
+        exclude_url_count_dict = None
+
+    return (
+        page_count,
+        in_root_page_count,
+        additional_url_count_dict,
+        exclude_url_count_dict,
+    )
